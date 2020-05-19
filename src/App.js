fbc8e3da4e6feea46a5634fcbeb5d1af4ddbc25b
dudjslsk <54700815+dudjslsk@users.noreply.github.com>
2020-05-19 17:41:01 +0900

    state

--- src/App.js
+++ src/App.js
@@ -13,48 +13,115 @@ class App extends React.Component{
       title: "",
       content: "",
       startDate: null,
-      StarTime: null,
+      startTime: null,
       endDate: null,
-      endTime: null
-    }
+      endTime: null 
+    } 
   }
 
+  onChangetext = (event) => {
+    this.setState({
+      [event.target.name] : event.target.value
+    })
+  }
+  //시작날짜
+  onChangeStartDate = (event) => {
+    this.setState({
+      startDate : event._d
+    })
+  }
+  //시작시간
+  onChangeStartTime = (event) => {
+    this.setState({
+      StartTime : event._d
+    })
+  }
+  //끝난날짜
+  onChangeEndDate = (event) => {
+    this.setState({
+      endDate : event._d
+    })
+  }
+  //끝난시간
+  onChangeEndTime = (event) => {
+    this.setState({
+      endTime : event._d
+    })
+  }
+
+
+
 render() {
+  
+
+  
+  console.log(this.state.title);
+  console.log(this.state.content);
+  console.log(this.state.startDate)
+  console.log(this.state.startTime)
+  console.log(this.state.endDate)
+  console.log(this.state.endTime)
+
+
+
   return (
     <div className="App">
       <div className="header"> TODO LIST</div>
       <Paper className="input_area" variant="outline" style={{padding: '10px'}}>
-      <div className="input_area"> 
-        <TextField label="제목" placeholder="제목을 입력해 주세요" size="normal" margin="normal" fullWidth required />
-        <TextField label="상세내용" size="normal" margin="normal" fullWidth multiline />
+      
+        <TextField label="제목" placeholder="제목을 입력해 주세요" size="normal" margin="normal" fullWidth required 
+         onChange={this.onChangetext} value={this.state.title} name="title"/>
+        <TextField label="상세내용" size="normal" margin="normal" fullWidth multiline 
+         onChange={this.onChangetext} value={this.state.content} name="content"/>
+
         <KeyboardDatePicker
-          disableTooldar
-          variant="inline"
-          format="yyyy/MM/DD/dd"
-          margin="normal"
-          label="시작 예정일"
-          onChange={(value)=>console.log(value)}
-          style={{widht:'100%'}}
-          KeyboardButtonProps={{
-            'aria-label' : 'change date',
-          }}
-        />
-        <KeyboardTimePicker
-          margin="normal"
-          label="시작시간"
-          variant="inline"
-          onChange={(value)=>console.log(value)}
-          style={{widht:'50%'}}
-          KeyboardButtonProps={{
-            'aria-label' : 'change time',
-          }}
-        />
-      </div>
+            disable="inline"
+            format="yyyy/MM/DD/dd"
+            margin="normal"
+            label="시작 예정일"
+            onChange={this.onChangeStartDate}
+            style={{ width: '50%' }}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+          <KeyboardTimePicker
+            margin="normal"
+            label="시작시간"
+            variant="inline"
+            onChange={this.onChangeStartTime}
+            style={{ width: '50%' }}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+
+          <KeyboardDatePicker
+            disable="inline"
+            format="yyyy/MM/DD/dd"
+            margin="normal"
+            label="끝난 날짜"
+            onChange={this.onChangeEndDate}
+            style={{ width: '50%' }}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+          <KeyboardTimePicker
+            margin="normal"
+            label="끝날 예정일"
+            variant="inline"
+            onChange={this.onChangeEndTime}
+            style={{ width: '50%' }}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
 
       </Paper>
       <div className="list_area"> 리스트 영역</div>
       <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright ⓒ 홍길동' +new Date().getFullYear()+'.'}
+        {'Copyright ⓒ 김종현' +new Date().getFullYear()+'.'}
       </Typography>
     </div>
   );
